1b37d92922a23f2a61680dbc058b27dff2f8f552
Alex Galvis <alex.galvis.sistemas@gmail.com>
2020-07-08 18:19:07 -0500

    init project

---
+++ src/main/java/co/com/gsdd/docker/config/util/DockerEnvException.java
@@ -0,0 +1,10 @@
+package co.com.gsdd.docker.config.util;
+
+public class DockerEnvException extends RuntimeException {
+
+    private static final long serialVersionUID = -3125442528503490796L;
+
+    public DockerEnvException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/test/java/co/com/gsdd/docker/config/util/DockerEnvLoaderTest.java
@@ -0,0 +1,14 @@
+package co.com.gsdd.docker.config.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+
+public class DockerEnvLoaderTest {
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "DOCKER_HOST", matches = "tcp://192.168.99.113:2376")
+    public void testGetDockerServiceIp() {
+        Assertions.assertEquals("tcp://192.168.99.113", DockerEnvLoader.getDockerServiceIp());
+    }
+}
