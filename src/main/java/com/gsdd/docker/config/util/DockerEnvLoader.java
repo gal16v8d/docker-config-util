03e602a1f8e6f63ab59549ded41ba58137a4bb27
Alex Galvis <alex.galvis.sistemas@gmail.com>
2021-12-05 23:05:39 -0500

    Fix sonar code smells

---
+++ src/main/java/com/gsdd/docker/config/util/DockerEnvLoader.java
@@ -0,0 +1,17 @@
+package com.gsdd.docker.config.util;
+
+import java.util.Optional;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class DockerEnvLoader {
+
+  public static String getDockerServiceIp() {
+    String dockerHost = System.getenv("DOCKER_HOST");
+    return Optional.ofNullable(dockerHost).map(host -> host.substring(0, host.lastIndexOf(':')))
+        .map(modifiedHost -> modifiedHost.substring(modifiedHost.lastIndexOf('/') + 1))
+        .orElseGet(() -> "localhost");
+  }
+
+}
